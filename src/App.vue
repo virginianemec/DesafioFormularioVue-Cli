c2a98f3e2ad75bbeedd3d3dcda24b551f19fbd8b
CENTRALSYS\vnemec <virginia.nemec@gmail.com>
2022-09-21 11:36:41 -0300

    Clear

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,6 @@
       :presentation_name="this.presentation_name"
       :workDescription="this.workDescription"
     ></HeaderComponent>
-    <!-- <FormComponent
-      :products="products"
-      @itemAdd="itemAddFromForm($event)"
-      @itemDelete="itemDeleteFromForm($event)"
-      @itemEdit="itemEditFromForm($event)"
-    ></FormComponent>-->
     <div class="div--container">
       <vue-form
         :state="formStateProducts"
@@ -44,7 +38,7 @@
             <field-messages name="price">
               <div>Ok.</div>
               <div slot="required">Ingrese un precio por favor.</div>
-              <div slot="priceValidator">El precio debe ser mayor a 0 (cero).</div>
+              <div slot="priceValidator">La comida sin TACC no debe costar mas de $1500</div>
               <div slot="min">El valor debe ser numerico mayor a cero.</div>
               <div slot="max">El valor debe ser numerico menor a 9999.</div>
             </field-messages>
@@ -85,35 +79,7 @@
         </div>
       </vue-form>
     </div>
-    <!-- <TableComponent
-      :products="products"
-      @itemDelete="itemDeleteFromTable($event)"
-      @itemEdit="itemEditFromFormTable($event)"
-    ></TableComponent>
-    -->
-    <div class="titleTable">LISTADO DE PRODUCTOS</div>
-    <div>
-      <table class="table">
-        <thead>
-          <tr class="table--tr">
-            <th>ID</th>
-            <th>NAME</th>
-            <th>PRICE</th>
-            <th>CATEGORY</th>
-            <th>A LA VENTA?</th>
-          </tr>
-        </thead>
-        <tbody v-for="product in products" :key="product.id">
-          <tr>
-            <td class="table--td">{{ product.id }}</td>
-            <td class="table--td">{{ product.name | nameFilter(product.name) }}</td>
-            <td class="table--td">{{ product.price | priceFilter(product.price) }}</td>
-            <td class="table--td">{{ product.category | categoryFilter }}</td>
-            <td class="table--td">{{ product.activo | isActivo(product.activo) }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <TableComponente :products="this.products"></TableComponente>
     <FooterComponent />
   </div>
 </template>
@@ -121,7 +87,7 @@
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
 import FooterComponent from "./components/FooterComponent.vue";
-//import FormComponent from "./components/FormComponent.vue";
+import TableComponente from "./components/TableComponente.vue";
 
 export default {
   name: "App",
@@ -146,21 +112,7 @@ export default {
   components: {
     HeaderComponent,
     FooterComponent,
-    //FormComponent,
-  },
-  filters: {
-    nameFilter(value) {
-      if (!value) return "";
-      value = value.toString();
-      return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
-    },
-    priceFilter(value) {
-      if (!value) return "$ No asignado.";
-      return `$ ${value}`;
-    },
-    isActivo(value) {
-      return value === 1 ? "SI" : "NO";
-    },
+    TableComponente,
   },
   methods: {
     itemAddFromForm() {
@@ -170,25 +122,13 @@ export default {
       this.resetProduct();
       console.log("add item");
     },
-    itemDeleteFromForm(data) {
-      const indexOfObject = this.products.findIndex(
-        (object) => object.id === data.id
-      );
-
-      this.products.splice(indexOfObject, 1);
-
-      console.log("itemdelete");
-    },
-    itemEditFromForm() {
-      const indexOfObject = this.products.findIndex(
-        (object) => object.id === this.product.id
-      );
-      this.products[indexOfObject].name = this.product.name;
-      this.products[indexOfObject].category = this.product.category;
-      this.products[indexOfObject].price = this.product.price;
-      this.products[indexOfObject].activo = this.product.activo;
-
-      console.log("hola");
+    productAdd() {
+      if (this.formStateProducts.$invalid) {
+        alert("Los datos no son correctos. Verifiquelos por favor.");
+        return;
+      }
+      alert("Guardamos el item");
+      this.itemAddFromForm();
     },
     resetProduct() {
       this.product = {
@@ -200,49 +140,21 @@ export default {
         activo: 1,
       };
     },
-    productAdd() {
-      if (this.formStateProducts.$invalid) {
-        alert("Los datos no son correctos. Verifiquelos por favor.");
-        return;
-      }
-      alert("Guardamos el item");
-      this.itemAddFromForm();
-    },
-    productDelete() {
-      /*if (this.formStateProducts.$invalid) {
-        alert("Los datos no son correctos. Verifiquelos por favor.");
-        return;
-      }*/
-      // Seguro quiere borrar el item?
-      alert("Borramos el item");
-      this.itemDeleteFromForm();
-    },
-    productEdit() {
-      if (this.formStateProducts.$invalid) {
-        alert("Los datos no son correctos. Verifiquelos por favor.");
-        return;
-      }
-      // Seguro quiere borrar el item?
-      alert("Modificamos el item");
-      this.itemEditFromForm();
-    },
-
     customValidator: function () {
       return this.productExists() ? false : true;
     },
     activoValidator() {
       return this.product.activo ? true : false;
     },
-    /* productNameValidator() {
-      let respuesta = false;
-
-      if (this.product.stock >= 18) {
-        respuesta = true;
+    priceValidator() {
+      let respuesta = true;
+      if (this.product.category === "Comida sin TACC") {
+        if (this.product.price > 1500) {
+          //si es sin tacc debiera valer menos de 1500.
+          respuesta = false;
+        }
       }
       return respuesta;
-    },*/
-    priceValidator() {
-      return true;
     },
     productExists() {
       let respuesta = false;
@@ -293,14 +205,6 @@ p {
   text-align: center;
   justify-content: space-around;
 }
-
-.titleTable {
-  background-color: #babcc2;
-  /* color: white;*/
-  text-align: center;
-  font-size: 15px;
-  padding-top: 10px;
-}
 .div--container {
   display: flex;
   flex-wrap: wrap;
@@ -308,35 +212,5 @@ p {
   gap: 10px;
   width: 100%;
   padding: 20px;
-}
-table {
-  width: 90%;
-  margin: auto;
-  padding: 25px;
-  margin-bottom: 20px;
-  border-radius: 15px;
-  box-shadow: 2px 2px 10px rgb(142, 142, 142);
-  transition: 0.3s;
-  color: #2c3e50;
-  text-align: left;
-}
-/*table:hover {
-  transform: scale(1.05);
-}*/
-th {
-  text-align: left;
-  font-size: 16px;
-  padding-left: 10px;
-}
-.table--tr {
-  color: #555555;
-  text-align: center;
-  font-size: 2.1rem;
-}
-.table--td {
-  padding-top: 10px;
-  color: #555555;
-  text-align: left;
-  font-size: 16px;
 }
 </style>
